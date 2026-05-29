5839c56b014d043917f6ae18bf488a2a186381eb
anajimenezmontes <a.jimenezmontes@studenti.unisa.it>
2026-05-29 11:39:26 +0200

    sub-task 4

--- task.py
+++ task.py
@@ -12,8 +12,9 @@ def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
     return np.linalg.solve(A, b)
 
 def correlazione_matrici(m1: list, m2: list) -> np.ndarray:
-    """Sub-task 4: Correlazione tra Matrici 2x2."""
-    pass
+    m1_flat = np.array(m1).flatten()
+    m2_flat = np.array(m2).flatten()
+    return np.corrcoef(m1_flat, m2_flat)
 
 def operazioni_elemento_per_elemento(v1: list) -> tuple:
     """Sub-task 5: Restituisce (seno, coseno, arcoseno, arcocoseno) elemento per elemento calcolati sul primo array."""
@@ -22,7 +23,7 @@ def operazioni_elemento_per_elemento(v1: list) -> tuple:
 
 def main():
     print("Sub-task 1:", prodotto_scalare([1, 2, 3], [4, 5, 6]))
-    print("Sub-task 1:", rango_matrice([[1, 2], [3, 4]]))
+    print("Sub-task 2:", rango_matrice([[1, 2], [3, 4]]))
     print("Sub-task 3:", risolvi_sistema_lineare([[2, 1], [1, 3]], [5, 7]))
     print("Sub-task 4:", correlazione_matrici([[1, 2], [3, 4]], [[2, 4], [6, 8]]))
     print("Sub-task 5:", operazioni_elemento_per_elemento([0, 0.5, 1, -0.5]))
